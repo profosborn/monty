6064e7fe3a84c2d316c7f83fabd838993924e5cc
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-20 15:08:32 +0000

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,1 +1,14 @@
+#include "monty.h"
 
+/**
+ * f_stack - Sets the stack mode.
+ *
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number (unused).
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    bus.lifi = 0;
+}
