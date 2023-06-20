1649ea0cfe9b090a8414536aaf0443579245810d
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-20 14:39:46 +0000

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,1 +1,12 @@
+#include "monty.h"
 
+/**
+ * f_nop - Does nothing.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+}
