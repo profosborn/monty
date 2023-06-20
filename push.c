c02af98a39adea668e8165d2357ffcaab00fd805
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-20 13:20:07 +0000

    Update push.c

--- push.c
+++ push.c
@@ -1,1 +1,48 @@
+#include "monty.h"
 
+/**
+ * f_push - Pushes an element onto the stack or queue.
+ * @head: Pointer to the head of the stack or queue.
+ * @counter: Line number in the file.
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int value, i = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			i++;
+
+		while (bus.arg[i] != '\0')
+		{
+			if (!isdigit(bus.arg[i]))
+				flag = 1;
+			i++;
+		}
+
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(bus.arg);
+
+	if (bus.lifi == 0)
+		addnode(head, value);
+	else
+		addqueue(head, value);
+}
