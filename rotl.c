2d3a6086b155bf11fc075606b68ddf686251ec15
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-20 15:05:15 +0000

    Update rotl.c

--- rotl.c
+++ rotl.c
@@ -1,1 +1,30 @@
+#include "monty.h"
 
+/**
+ * f_rotl - Rotates the stack to the top.
+ *
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number (unused).
+ */
+void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+    stack_t *tmp = *head, *aux;
+
+    if (*head == NULL || (*head)->next == NULL)
+    {
+        return;
+    }
+
+    aux = (*head)->next;
+    aux->prev = NULL;
+
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+
+    tmp->next = *head;
+    (*head)->next = NULL;
+    (*head)->prev = tmp;
+    (*head) = aux;
+}
