5cbfc1b4de98eae716d5b0b02f984385ffe033ab
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-20 14:59:26 +0000

    Update mod.c

--- mod.c
+++ mod.c
@@ -1,1 +1,45 @@
+#include "monty.h"
 
+/**
+ * f_mod - Computes the remainder of the division of the second
+ *         top element of the stack by the top element of the stack.
+ *
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number.
+ */
+void f_mod(stack_t **head, unsigned int counter)
+{
+    stack_t *current;
+    int stack_size = 0, remainder;
+
+    current = *head;
+    while (current)
+    {
+        current = current->next;
+        stack_size++;
+    }
+
+    if (stack_size < 2)
+    {
+        fprintf(stderr, "L%d: Unable to perform modulo, stack is too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    current = *head;
+    if (current->n == 0)
+    {
+        fprintf(stderr, "L%d: Division by zero\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    remainder = current->next->n % current->n;
+    current->next->n = remainder;
+    *head = current->next;
+    free(current);
+}
