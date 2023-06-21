4f83f7a6788016c9ff6fe83260af45ba16f347ec
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:24:03 +0000

    Update pchar.c

--- pchar.c
+++ pchar.c
@@ -8,26 +8,26 @@
  */
 void f_pchar(stack_t **head, unsigned int counter)
 {
-    stack_t *current;
+	stack_t *current;
 
-    current = *head;
-    if (!current)
-    {
-        fprintf(stderr, "L%d: Unable to print character, stack is empty\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	current = *head;
+	if (!current)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    if (current->n > 127 || current->n < 0)
-    {
-        fprintf(stderr, "L%d: Unable to print character, value is out of range\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	if (current->n < 0 || current->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%c\n", current->n);
+	printf("%c\n", current->n);
 }
