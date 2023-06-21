68b17633acc8b473f868aa52e38e1db95331510d
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:46:13 +0000

    Update push.c

--- push.c
+++ push.c
@@ -7,26 +7,24 @@
  */
 void f_push(stack_t **head, unsigned int counter)
 {
-    int value, i = 0;
-    stack_t *new_node;
+    int value;
 
     if (bus.arg)
     {
-        if (bus.arg[0] == '-')
-            i++;
-
-        while (bus.arg[i] != '\0')
+        if (!isdigit(*bus.arg) && *bus.arg != '-' && *bus.arg != '+')
         {
-            if (!isdigit(bus.arg[i]))
-            {
-                fprintf(stderr, "L%u: usage: push integer\n", counter);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*head);
-                exit(EXIT_FAILURE);
-            }
-            i++;
+            fprintf(stderr, "L%u: usage: push integer\n", counter);
+            fclose(bus.file);
+            free(bus.content);
+            free_stack(*head);
+            exit(EXIT_FAILURE);
         }
+
+        value = atoi(bus.arg);
+        if (bus.lifo == 0)
+            addnode(head, value); // Add to the top of the stack (LIFO)
+        else
+            addqueue(head, value); // Add to the end of the stack (FIFO)
     }
     else
     {
@@ -35,30 +33,5 @@ void f_push(stack_t **head, unsigned int counter)
         free(bus.content);
         free_stack(*head);
         exit(EXIT_FAILURE);
-    }
-
-    value = atoi(bus.arg);
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = value;
-    new_node->prev = NULL;
-
-    if (*head == NULL)
-    {
-        new_node->next = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        new_node->next = *head;
-        (*head)->prev = new_node;
-        *head = new_node;
     }
 }
