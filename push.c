462aa1abb458a39c7a12264183d091fd6812e01a
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:56:49 +0000

    Update push.c

--- push.c
+++ push.c
@@ -7,48 +7,50 @@
  */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int value, i = 0;
+	int value;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			i++;
-
-		while (bus.arg[i] != '\0')
-		{
-			if (!isdigit(bus.arg[i]))
-			{
-				fprintf(stderr, "L%u: usage: push integer\n", counter);
-				fclose(bus.file);
-				free(bus.content);
-				free_stack(*head);
-				exit(EXIT_FAILURE);
-			}
-			i++;
-		}
-	}
-	else
+	if (!bus.arg)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 
 	value = atoi(bus.arg);
 
-	if (value == 0 && strcmp(bus.arg, "0") != 0)
+	if (!is_valid_integer(bus.arg, value))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 
-	if (bus.lifi == 0)
+	if (bus.lifo == 0)
 		addnode(head, value);
 	else
 		addqueue(head, value);
+}
+
+/**
+ * is_valid_integer - Checks if a string represents a valid integer.
+ * @str: String to be checked.
+ * @value: Integer value obtained from the string.
+ * Return: 1 if the string is a valid integer, 0 otherwise.
+ */
+int is_valid_integer(char *str, int value)
+{
+	int i = 0;
+
+	if (str[i] == '-')
+		i++;
+
+	while (str[i] != '\0')
+	{
+		if (!isdigit(str[i]))
+			return 0;
+		i++;
+	}
+
+	if (value == 0 && strcmp(str, "0") != 0)
+		return 0;
+
+	return 1;
 }
