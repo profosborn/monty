cb2df5c17b05b1d5b641e255ca9e9f6cedead9ca
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:44:08 +0000

    Update push.c

--- push.c
+++ push.c
@@ -7,48 +7,58 @@
  */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int value, i = 0;
+    int value, i = 0;
+    stack_t *new_node;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			i++;
+    if (bus.arg)
+    {
+        if (bus.arg[0] == '-')
+            i++;
 
-		while (bus.arg[i] != '\0')
-		{
-			if (!isdigit(bus.arg[i]))
-			{
-				fprintf(stderr, "L%u: usage: push integer\n", counter);
-				fclose(bus.file);
-				free(bus.content);
-				free_stack(*head);
-				exit(EXIT_FAILURE);
-			}
-			i++;
-		}
-	}
-	else
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
+        while (bus.arg[i] != '\0')
+        {
+            if (!isdigit(bus.arg[i]))
+            {
+                fprintf(stderr, "L%u: usage: push integer\n", counter);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+            }
+            i++;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
 
-	value = atoi(bus.arg);
+    value = atoi(bus.arg);
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = value;
+    new_node->prev = NULL;
 
-	if (value == 0 && strcmp(bus.arg, "0") != 0)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-
-	if (bus.lifi == 0)
-		addnode(head, value);
-	else
-		addqueue(head, value);
+    if (*head == NULL)
+    {
+        new_node->next = NULL;
+        *head = new_node;
+    }
+    else
+    {
+        new_node->next = *head;
+        (*head)->prev = new_node;
+        *head = new_node;
+    }
 }
