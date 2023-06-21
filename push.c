1ec0a3f7ef6e34f028a9917cd111a2c90fc707a6
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:58:24 +0000

    Update push.c

--- push.c
+++ push.c
@@ -7,50 +7,58 @@
  */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int value;
+    int value, i = 0;
+    stack_t *new_node;
 
-	if (!bus.arg)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		exit(EXIT_FAILURE);
-	}
+    if (bus.arg)
+    {
+        if (bus.arg[0] == '-')
+            i++;
 
-	value = atoi(bus.arg);
+        while (bus.arg[i] != '\0')
+        {
+            if (!isdigit(bus.arg[i]))
+            {
+                fprintf(stderr, "L%u: usage: push integer\n", counter);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+            }
+            i++;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
 
-	if (!is_valid_integer(bus.arg, value))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", counter);
-		exit(EXIT_FAILURE);
-	}
+    value = atoi(bus.arg);
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = value;
+    new_node->prev = NULL;
 
-	if (bus.lifo == 0)
-		addnode(head, value);
-	else
-		addqueue(head, value);
-}
-
-/**
- * is_valid_integer - Checks if a string represents a valid integer.
- * @str: String to be checked.
- * @value: Integer value obtained from the string.
- * Return: 1 if the string is a valid integer, 0 otherwise.
- */
-int is_valid_integer(char *str, int value)
-{
-	int i = 0;
-
-	if (str[i] == '-')
-		i++;
-
-	while (str[i] != '\0')
-	{
-		if (!isdigit(str[i]))
-			return 0;
-		i++;
-	}
-
-	if (value == 0 && strcmp(str, "0") != 0)
-		return 0;
-
-	return 1;
+    if (*head == NULL)
+    {
+        new_node->next = NULL;
+        *head = new_node;
+    }
+    else
+    {
+        new_node->next = *head;
+        (*head)->prev = new_node;
+        *head = new_node;
+    }
 }
