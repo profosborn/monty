5db1096c89addd1c5d7b66374cfd6b9b378c37ba
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-06-21 09:15:34 +0000

    Update div.c

--- div.c
+++ div.c
@@ -1,43 +1,44 @@
 #include "monty.h"
 
 /**
- * f_div - Performs division of the top two elements of the stack.
+ * f_div - Divides the second top element
+ *           of the stack by the top element.
  * @head: Pointer to the head of the stack.
  * @counter: Line number.
  */
 void f_div(stack_t **head, unsigned int counter)
 {
-    stack_t *current;
-    int stack_size = 0, quotient;
+	stack_t *current;
+	int stack_size = 0, quotient;
 
-    current = *head;
-    while (current)
-    {
-        current = current->next;
-        stack_size++;
-    }
+	current = *head;
+	while (current)
+	{
+		current = current->next;
+		stack_size++;
+	}
 
-    if (stack_size < 2)
-    {
-        fprintf(stderr, "L%d: Unable to perform division, stack is too short\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	if (stack_size < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    current = *head;
-    if (current->n == 0)
-    {
-        fprintf(stderr, "L%d: Division by zero is not allowed\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	current = *head;
+	if (current->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    quotient = current->next->n / current->n;
-    current->next->n = quotient;
-    *head = current->next;
-    free(current);
+	quotient = current->next->n / current->n;
+	current->next->n = quotient;
+	*head = current->next;
+	free(current);
 }
